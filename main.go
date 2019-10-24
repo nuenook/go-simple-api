2a631d7b9d0a821d5fa1eee494acd6ccc3e7f3ca
nuenook <nook.ittipon@gmail.com>
2019-10-24 15:31:59 +0700

    READ operation

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Id string `json: "Id"`
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+
+func handleRequests() {
+	// http.HandleFunc("/", homePage)
+	// http.HandleFunc("/articles", returnAllArticles)
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Fprintf(w, "key: "+ key)
+
+	for _, article := range Articles {
+		
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)	
+		}
+	}
+}
+
+func main() {
+
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Articles = []Article{
+        Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Id: "2",Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	
+	handleRequests()
+}
