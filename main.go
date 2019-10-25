3bb9cd2ea3b3820de4a90a002847bad2a7a33f15
nuenook <nook.ittipon@gmail.com>
2019-10-25 18:07:45 +0700

    post method

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"encoding/json"
+
 	"github.com/gorilla/mux"
 )
 
 type Article struct {
-	Id string `json: "Id"`
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
+	Id      string `json: "Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
@@ -23,7 +25,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-
 func handleRequests() {
 	// http.HandleFunc("/", homePage)
 	// http.HandleFunc("/articles", returnAllArticles)
@@ -32,6 +33,7 @@ func handleRequests() {
 
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
 	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
@@ -46,23 +48,38 @@ func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
 
-	fmt.Fprintf(w, "key: "+ key)
+	fmt.Fprintf(w, "key: "+key)
 
 	for _, article := range Articles {
-		
+
 		if article.Id == key {
-			json.NewEncoder(w).Encode(article)	
+			json.NewEncoder(w).Encode(article)
 		}
 	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	err := json.Unmarshal(reqBody, &article)
+
+	if err != nil {
+		fmt.Println("err %+v", err)
+		return
+	}
+
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
 }
 
 func main() {
 
 	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article{
-        Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Id: "2",Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
-	
+
 	handleRequests()
 }
